ca14c03d8ce8443c3ee255f98fdabd2ee42abf20
Stefan Hipfel <stefan.hipfel@googlemail.com>
2018-05-17 13:50:47 +0200

    Removes aws instance profile when using wal-e retain func

--- postgres-appliance/postgres_backup.sh
+++ postgres-appliance/postgres_backup.sh
@@ -25,8 +25,7 @@ IN_RECOVERY=$(psql -tXqAc "select pg_is_in_recovery()")
 # leave at least 2 base backups before creating a new one
 [[ "$NUM_TO_RETAIN" -lt 2 ]] && NUM_TO_RETAIN=2
 
-# --aws-instance-profile flag is just ignored when running in GCE.
-#envdir "${WALE_ENV_DIR}" wal-e --aws-instance-profile delete --confirm retain "${NUM_TO_RETAIN}"
+envdir "${WALE_ENV_DIR}" wal-e delete --confirm retain "${NUM_TO_RETAIN}"
 
 # Ensure we don't have more workes than CPU's
 POOL_SIZE=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)
